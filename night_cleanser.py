934b000dfab27d44bf2fc8d76e2a0ce99d388f81
Tom Rice <t.rice90@gmail.com>
2012-08-13 16:53:26 -1000

    Replaced selective_flag_scrubber() with ''2() due to speed gains.
    This was an awesome upgrade.

--- night_cleanser.py
+++ night_cleanser.py
@@ -174,82 +174,6 @@ def null_cleanser_grader(data, timestamps, j_ratio, h_ratio, k_ratio,
 
 
 def selective_flag_scrubber(data, lookup, threshold=0.1, 
-                            null=np.double(-9.99999488e+08)):
-    """ 
-    Scrubs error-flagged points from data of normally-unflagged stars.
-
-    Goes through a data file star by star, replacing error-flagged
-    data with `null`, only in stars where the ratio of flagged
-    data to all data is below `threshold`.
-    
-    If a star has proportionally more flagged data than `threshold`, 
-    its data is left intact. The purpose of this is to purify lightcurves
-    that are polluted by just a couple of bad points.
-
-    Parameters
-    ----------
-    data : atpy.Table
-        Table that contains all the photometry data.
-    lookup : atpy.Table
-        A statistics spreadsheet (produced by spread3.py) that
-        contains only stars you want to clean up, as well
-        as columns for N_j_noflags, N_j_info, etc. (for all 3 bands).
-    threshold : float, optional
-        Upper limit on how much flagged data to remove.
-        If a star has more flagged data than this, its data are 
-        left untouched.
-        Default: 0.1
-    null : float, optional
-        What value to use as a 'null' when cleansing data.
-        Default value -9.99999e+08 (as used by WSA).
-
-    Returns
-    -------
-    scrubbed_data : atpy.Table
-        Table with selected flagged data scrubbed.
-
-    """
-
-    # Make a copy of the data table
-    scrubbed_data = data.where(data.SOURCEID != 0)
-
-    # Calculate flag ratio arrays
-
-    jflag_ratio = (lookup.N_j_info)/(lookup.N_j_noflag + lookup.N_j_info)
-    hflag_ratio = (lookup.N_h_info)/(lookup.N_h_noflag + lookup.N_h_info)
-    kflag_ratio = (lookup.N_k_info)/(lookup.N_k_noflag + lookup.N_k_info)
-
-    for s, jfr, hfr, kfr in zip(lookup.SOURCEID, 
-                                jflag_ratio, hflag_ratio, kflag_ratio):
-        
-#        s_table = data_cut( data, s )
-
-#        jt = band_cut(s_table, 'j')
-#        ht = band_cut(s_table, 'h')
-#        kt = band_cut(s_table, 'k')
-
-        rdict =  {'j':jfr, 'h':hfr, 'k':kfr}
-        
-        for band in ['j', 'h', 'k']:
-
-            # Check to see if this star and band qualifies for correction
-            if rdict[band] > threshold:
-                continue
-
-            print "Scrubbing star %s at %s band" % (str(s), band.upper())
-
-            col = band.upper()+"APERMAG3"
-            pperrbits = band.upper()+"PPERRBITS"
-
-            # otherwise, looks like we qualify, so let's run our stuff:
-            # For this star's data, null out the nonzero-flagged points
-            scrubbed_data.data[col][(scrubbed_data.data[pperrbits] > 0) & 
-                                    (scrubbed_data.SOURCEID == s)] = null
-
-
-    return scrubbed_data
-
-def selective_flag_scrubber2(data, lookup, threshold=0.1, 
                              null=np.double(-9.99999488e+08)):
     """ 
     Scrubs error-flagged points from data of normally-unflagged stars.
@@ -285,6 +209,10 @@ def selective_flag_scrubber2(data, lookup, threshold=0.1,
     scrubbed_data : atpy.Table
         Table with selected flagged data scrubbed.
 
+    Notes
+    -----
+    Formerly known as selective_flag_scrubber2().
+        
     """
 
 
