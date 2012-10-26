2f84661c9cf532fa5568dc3aee3015c6fdb25fa1
Tom Rice <t.rice90@gmail.com>
2012-10-26 16:41:36 -0700

    Now running stats on season1 data.

--- variability_script_orion.py
+++ variability_script_orion.py
@@ -28,7 +28,7 @@ print "the data into. Make sure to use the same number for both functions!!"
 path = '/home/tom/reu/ORION/DATA/'
 path2= path+'spreadsheet/'
 
-data = atpy.Table('/home/tom/reu/ORION/DATA/maxvars_data.fits')
+data = atpy.Table('/home/tom/reu/ORION/DATA/maxvars_data_s1.fits')
 #data = atpy.Table('/home/tom/reu/ORION/DATA/fdece_graded_clipped0.8_scrubbed0.1_dusted0.5.fits')
 #data = atpy.Table('/home/tom/reu/ORION/DATA/fdece_graded_clipped0.8_scrubbed0.1_dusted0.5.fits')
 #data = atpy.Table('/home/tom/reu/ORION/DATA/constantstars_073112_data_errorcorrected.fits')
