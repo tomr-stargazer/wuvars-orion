8612fda0dedbc444ea651a0c539f189f6ee7b7aa
Tom Rice <t.rice90@gmail.com>
2012-11-01 20:07:02 -0700

    Substantially revised parts of osc.
    Debug counts are gone; now we have "result"-y print statements phrased in
    question-and-answer form!

--- official_star_counter.py
+++ official_star_counter.py
@@ -5,11 +5,19 @@ set of filters.
 
 """ 
 
+from __future__ import division
+
 import numpy as np
 
 import atpy
 
+import periodic_selector as ps
+
 spread = atpy.Table("/home/tom/reu/ORION/DATA/fdece_graded_clipped0.8_scrubbed0.1_dusted0.5_spread.fits")
+
+maxvars_spread_per = atpy.Table("/media/storage/Documents/Research/reu/ORION/DATA/maxvars_data_statsper.fits")
+maxvars_s1_spread_per = atpy.Table("/media/storage/Documents/Research/reu/ORION/DATA/maxvars_data_s1_statsper.fits")
+
 
 # Number of detected sources in the dataset
 print "Number of detected sources in the dataset:"
@@ -20,16 +28,13 @@ print len(spread)
 #  At least 50 observations (as measured by Stetson_N or just per band)
 #  
 
-minimum = spread.where(spread.Stetson_N > 50)
-alt_minimum = spread.where((spread.N_j >= 50) |
-                           (spread.N_k >= 50) |
-                           (spread.N_h >= 50) )
+minimum = spread.where((spread.N_j >= 50) |
+                       (spread.N_k >= 50) |
+                       (spread.N_h >= 50) )
 
 print "Number of stars that meet absolute minimum considerations for valid data:"
 print "(i.e., have at least 50 recorded observations in at least one band)"
 print len(minimum)
-print len(alt_minimum)
-
 
 # Automatic variables
 # Criteria:
@@ -49,33 +54,7 @@ maxvars = sp.where( (sp.Stetson > 1) & (
         (sp.N_h >= 50) ) )
 print "Maximum possible number of variables: %d" % len(maxvars)
 
-jvars = sp.where(   
-    (sp.Stetson > 1) & (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
-        (sp.j_mean > 11) & (sp.j_mean < 17) & # J
-        (sp.N_j_info == 0) ) )                # J
-
-print "number of autovariables that have qualifying J bands: %d" % len(jvars)
-
-hvars = sp.where(   
-    (sp.Stetson > 1) & (
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 17) & # H
-        (sp.N_h_info == 0) ) )                # H
-
-print "number of autovariables that have qualifying H bands: %d" % len(hvars)
-
-kvars = sp.where(   
-    (sp.Stetson > 1) & (
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
-        (sp.k_mean > 11) & (sp.k_mean < 17) & # K
-        (sp.N_k_info == 0) ) )                # K
-
-print "number of autovariables that have qualifying K bands: %d" % len(kvars)
-
-print "Maximum number of autovariables: %d" % (len(jvars)+len(hvars)+len(kvars))
-
-autovars = sp.where( 
+autovars_old = sp.where( 
     (sp.Stetson > 1) & ( (
         (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
         (sp.j_mean > 11) & (sp.j_mean < 17) & # J
@@ -86,6 +65,17 @@ autovars = sp.where(
         (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
         (sp.k_mean > 11) & (sp.k_mean < 16) & # K
         (sp.N_k_info == 0) ) ) )              # K
+
+autocandidates_old = sp.where( (
+        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
+        (sp.j_mean > 11) & (sp.j_mean < 17) & # J
+        (sp.N_j_info == 0) ) | (              # J
+        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
+        (sp.h_mean > 11) & (sp.h_mean < 16) & # H
+        (sp.N_h_info == 0) ) | (              # H
+        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
+        (sp.k_mean > 11) & (sp.k_mean < 16) & # K
+        (sp.N_k_info == 0) ) )                # K
 
 # "True" variability criterion has two cases:
 # 1. All 3 bands are quality, and S > 1 (this is identical to CygOB7), or
@@ -136,16 +126,6 @@ autovars_true = sp.where( case1 | case2 )
 
 autovars_strict = sp.where( case1 )
 
-autocandidates = sp.where( (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
-        (sp.j_mean > 11) & (sp.j_mean < 17) & # J
-        (sp.N_j_info == 0) ) | (              # J
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & # H
-        (sp.N_h_info == 0) ) | (              # H
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
-        (sp.k_mean > 11) & (sp.k_mean < 16) & # K
-        (sp.N_k_info == 0) ) )                # K
 
 # Now, to count how many stars have quality that meets "autovars_true".
 
@@ -191,16 +171,13 @@ autocan_true = sp.where( cand_case1 | cand_case2 )
 
 autocan_strict = sp.where( cand_case1 )
 
-
-print "Number of automatically detected variables: %d" % len(autovars)
-print "Number of stars that meet autoquality cuts: %d" % len(autocandidates)
-print " "
-print "Number of TRUE autovariables: %d" % len(autovars_true)
-print "Number of TRUE autocandidates: %d" % len(autocan_true)
+print "Number of stars automatically classed as variables: %d" % len(autovars_true)
+print "Number of stars that have the data quality for auto-classification: %d" % len(autocan_true)
 
 subjectives = maxvars.where( ~np.in1d(maxvars.SOURCEID, autovars_true.SOURCEID))
 
-print "Number of stars requiring subjective verification: %d" % len(subjectives)
+print ""
+print "Number of probably-variable stars requiring subjective verification due to imperfect data quality: %d" % len(subjectives)
 
 # Now let's count stars that meet our strict criteria in ALL 3 bands
 
@@ -208,3 +185,41 @@ print ""
 
 print "Number of STRICT autovariables: %d" % len(autovars_strict)
 print "Number of STRICT autocandidates: %d" % len(autocan_strict)
+
+print ""
+
+print " Q: Statistically, what fraction of our stars are variables?"
+print " A: %.2f%s, drawn from the tightest-controlled sample;" % (len(autovars_strict)/len(autocan_strict) * 100, r"%")
+print "    %.2f%s, drawn from a looser sample." % (len(autovars_true)/len(autocan_true) * 100, r"%")
+
+# Now for periodicity analysis, which relies on periodic_selector
+
+# THIS WON'T WORK, we have to bring in the maxvars_data_statsper which has been period-analyzed, and then do cuts of it that correspond to autovars, etc
+#autovars_true_periodic = ps.periodic_selector(autovars_true)
+#autovars_strict_periodic = ps.periodic_selector(autovars_true)
+
+periodics_s123 = ps.periodic_selector(maxvars_spread_per)
+periodics_s1 = ps.periodic_selector(maxvars_s1_spread_per)
+
+autovars_true_periodics = autovars_true.where( 
+    np.in1d(autovars_true.SOURCEID, periodics_s123.SOURCEID) |
+    np.in1d(autovars_true.SOURCEID, periodics_s1.SOURCEID) )
+
+autovars_strict_periodics = autovars_strict.where(
+    np.in1d(autovars_strict.SOURCEID, periodics_s123.SOURCEID) |
+    np.in1d(autovars_strict.SOURCEID, periodics_s1.SOURCEID) )
+
+print ""
+print "Number of autovariables that are periodic: %d" % len(autovars_true_periodics)
+print "Number of STRICT autovariables that are periodic: %d" % len(autovars_strict_periodics)
+print ""
+
+
+print " Q: Statistically, what fraction of our variables are periodic?"
+print " A: %.2f%s, drawn from the tightest-controlled sample;" % (len(autovars_strict_periodics)/len(autovars_strict) * 100, r"%")
+print "    %.2f%s, drawn from a looser sample." % (len(autovars_true_periodics)/len(autovars_true) * 100, r"%")
+
+print ""
+print " Q: What fraction of stars in this dataset are periodic variables?"
+print " A: %.2f%s, drawn from the tightest-controlled sample;" % (len(autovars_strict_periodics)/len(autocan_strict) * 100, r"%")
+print "    %.2f%s, drawn from a looser sample." % (len(autovars_true_periodics)/len(autocan_true) * 100, r"%")
