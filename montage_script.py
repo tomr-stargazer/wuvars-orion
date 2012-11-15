2a457e9e3181d516e89db2027bcc5a64c795b264
Tom Rice <t.rice90@gmail.com>
2012-11-14 23:38:16 -0800

    Added functions (hackish) to make books for the confirmed variables.

--- montage_script.py
+++ montage_script.py
@@ -15,9 +15,17 @@ path1 = "/home/tom/reu/ORION/DATA/subjective/periodic_book/"
 path2 = "/home/tom/reu/ORION/DATA/subjective/periodic_book/glued/"
 path3 = "/home/tom/reu/ORION/DATA/subjective/nonperiodic_book/"
 
+path4 = '/home/tom/reu/ORION/DATA/subjective/confirmed_book/periodic/'
+path5 = '/home/tom/reu/ORION/DATA/subjective/confirmed_book/periodic/glued/'
+path6 = '/home/tom/reu/ORION/DATA/subjective/confirmed_book/nonperiodic/'
+
 subjective_periodics = atpy.Table("/home/tom/reu/ORION/DATA/subjective/subjective_periodic_candidate_spreadsheet.fits")
 
+conf_subj_periodics = atpy.Table("/home/tom/reu/ORION/DATA/subjective/subjective_periodics_confirmed_spread.fits")
+
 subjective_nonpers = atpy.Table("/home/tom/reu/ORION/DATA/subjective/subjective_nonperiod_candidate_spreadsheet.fits")
+
+conf_subj_nonpers = atpy.Table("/home/tom/reu/ORION/DATA/subjective/subjective_nonpers_confirmed_spread.fits")
 
 data = atpy.Table('/home/tom/reu/ORION/DATA/fdece_graded_clipped0.8_scrubbed0.1_dusted0.5.fits')
 
@@ -60,5 +68,47 @@ def gen_nonper_lc(start=0):
         plot3.graded_lc(data, s, abridged=True, color_slope=True, 
                         timecolor=True,
                         outfile=path3+"%s_lc.png"%str(s))
+
+    return
+
+def gen_conf_periodic_plots(start=0):
+    """ 
+    Makes glued 3-panel plots for all of the periodic variable candidates
+    """
+
+    for s in conf_subj_periodics.SOURCEID[start:]:
+        # Let's make 3 plots. LC, folded, and pgram. Save em all into a place.
+
+        plot3.graded_lc(data, s, abridged=True, color_slope=True, 
+                        timecolor=True,
+                        outfile=path4+"%s_lc.png"%str(s))
+
+        plot3.graded_phase(data, s, timecolor='time', 
+                           period=conf_subj_periodics.best_period[
+                conf_subj_periodics.SOURCEID==s][0], 
+                           color_slope=True, outfile=path4+"%s_phase.png"%str(s))
+
+        try:
+            plot3.lsp_power(data, s, outfile=path4+"%s_pgram.png"%str(s))
+        except Exception, e:
+            print "periodogram failed for %s" % str(s)
+            print e
+
+
+        # now glue em together!
+
+        call(["montage","-mode", "concatenate", "-tile", "2x", 
+             path4+"%s_*.png" % str(s), path5+"%s-glued.png" % str(s) ])
+
+        return
+
+def gen_conf_nonper_lc(start=0):
+    """ Makes lightcurves for nonperiodic variable candidates. """
+
+    for s in conf_subj_nonpers.SOURCEID[start:]:
+
+        plot3.graded_lc(data, s, abridged=True, color_slope=True, 
+                        timecolor=True,
+                        outfile=path6+"%s_lc.png"%str(s))
 
     return
