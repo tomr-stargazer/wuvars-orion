5638d0e71fe9f8ea16e46c011901b2726c4c47ab
Tom Rice <t.rice90@gmail.com>
2012-11-19 17:27:02 -0800

    Finished the f_hist_periods(), working on basic maps.

--- figure_maker.py
+++ figure_maker.py
@@ -16,6 +16,11 @@ from montage_script import conf_subj_periodics, conf_subj_nonpers
 
 # As a test, let's make a histogram of periods.
 
+# Guide to variable names:
+# Currently, we're using 
+# `conf_subj_periodics` for subjective periodics,
+# `autovars_true_periodics` for the superset of automatically detected and pristine/strict
+
 # Filled: strict dudes
 # Cross-hatched: autovars_true
 # open: subjectives
@@ -34,14 +39,68 @@ def f_hist_periods():
          label=r"Periodics that needed subjective confirmation", 
              figure=fig)
 
+    # Second group
     plt.hist(autovars_true_periods.best_period, bins=22, range=[2,46], 
              color='b', label=r"Periodics with $\geq$ 1 pristine band",
              figure=fig)
 
+    # Third group
     plt.hist(autovars_strict_periods.best_period, bins=22, range=[2,46], 
              color='r', label="Periodics in strictest sample: 3 pristine bands",
              figure=fig)
 
     plt.legend()
     
+    plt.xlabel("Period (days)")
+    plt.ylabel("Number of periodic stars")
+    
     plt.show()
+
+    
+def f_map_periods():
+    """
+    A figure showing positions of periodic variables.
+
+    At some point I'll think more carefully about which maps I actually need
+    to produce, and whether APLpy is necessary for all of them.
+
+    """
+
+    fig = plt.figure()
+
+    # First group
+    plt.plot( np.degrees(conf_subj_periodics.RA),
+              np.degrees(conf_subj_periodics.DEC), 'wo', 
+              label=r"Periodics that needed subjective confirmation",
+              figure=fig)
+    
+    # Second group
+    plt.plot( np.degrees(autovars_true_periodics.RA),
+              np.degrees(autovars_true_periodics.DEC), 'bo',
+              label=r"Periodics with $\geq$ 1 pristine band",
+              figure=fig)
+
+    # Third group
+    plt.plot( np.degrees(autovars_strict_periodics.RA),
+              np.degrees(autovars_strict_periodics.DEC), 'ro',
+              label="Periodics in strictest sample: 3 pristine bands",
+              figure=fig)
+
+    # Right ascension is plotted increasing right-to-left, conventionally
+    plt.gca().invert_xaxis()
+
+    plt.legend()
+
+    plt.show()
+
+
+def f_map_variables():
+    """
+    A figure showing positions of all variables.
+
+    At some point I'll think more carefully about which maps I actually need
+    to produce, and whether APLpy is necessary for all of them.
+
+    """
+    
+    pass
