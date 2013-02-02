132e1513f9a58f8327ffd7d865a49d78691e51ce
Tom Rice <t.rice90@gmail.com>
2013-02-01 18:01:07 -0800

    Started (unfinished) work  on tablemater(). Keep working, Tom

--- orion_tablemate.py
+++ orion_tablemate.py
@@ -14,6 +14,12 @@ ago in `match.py` is expected.
 
 """
 
+import numpy as np
+
+import atpy
+
+import official_star_counter as osc
+
 # I think I'm gonna have to make a Table_Parameters class
 class TableParameters(object):
     """
@@ -21,16 +27,17 @@ class TableParameters(object):
     
     """
     
-    def __init__(self,  path, alias, full_name, 
-                 ra_cols, ra_fmt, dec_cols, dec_fmt,
-                 name_col, max_match = 1.0):
+    def __init__(self, path, alias, full_name, 
+                 ra_cols, dec_cols, radec_fmt,
+                 name_col, max_match = 1.0, data=None):
         """
         Initializing method.
 
         Parameters
         ----------
-        path : str
+        path : str or None
             The location of the file containing our table data.
+            Only use None if `data` is specified.
         alias : str
             A consise string describing the table's name.
         full_name : str
@@ -45,17 +52,24 @@ class TableParameters(object):
         max_match : float, optional
             Largest possible match radius for this table, in arcseconds. 
             Default value: 1.0
+        data : atpy.Table
         
         """
 
         # We're gonna check to make sure that the provided filepath is valid.
         # If it's not, throw some exceptions around.
-        try:
-            self.data = atpy.Table(path)
-        except IOError:
-            raise IOError("File '%s' not found" % path)
-        except Exception, e:
-            raise e
+        if data = None:
+            try:
+                self.data = atpy.Table(path)
+            except IOError:
+                raise IOError("File '%s' not found" % path)
+            except Exception, e:
+                raise e
+        elif type(data) is atpy.basetable.Table:
+            self.data = data
+            self.path = None
+        else:
+            raise Exception("Data in `data` is invalid.")
 
         # Is this sloppy? See http://stackoverflow.com/questions/12191075/is-there-a-shortcut-for-self-somevariable-somevariable-in-a-python-class-con
         self.path = path
@@ -68,9 +82,71 @@ class TableParameters(object):
 
 
 
+def tablemater(primary_table, secondary_table_list):
+    """ 
+    Creates the mated table.
+
+    Parameters
+    ----------
+    primary_table : TableParameters instance
+        The primary catalog of stars we're matching to,
+        as well any important associated information.
+    secondary_table_list : array of TableParameters instance
+        A list containing the other tables we're matching
+        to our primary table, with their parameters.
+    
+    Returns
+    -------
+    mated_table : atpy.Table
+        A catalog where each row corresponds to a star in `primary_table`
+        and contains a cross-identification for the secondary_tables 
+        in each column (or a sentinel value indicating failure to match).
+        
+    """
+
+    # First, 
+
+    for st in secondary_table_list:
+        
+        # Determine or construct decimal-degree RA and Dec columns.
+        if 'sex' in st.radec_fmt.lower():
+            # For each row 
+            pass # until I implement the above
+        elif len(ra_cols) == len(dec_cols) == 1:
+            if 'deg' in 
+                
+
+
+
 def test():
     """
     Uses the above code to generate a simple test match-table.
 
     """
-                     
+       
+    # WFCAM Orion Variables - autovars, strict
+    wov_avs_data = atpy.Table(osc.autovars_strict)
+
+    chs = TableParameters(
+        # path
+        "/home/tom/reu/ORION/DATA/carpenter_table4.fits", 
+        # alias
+        "CHS2001",
+        # full_name
+        "Table 4. 'Near-Infrared Variable Stars', from 'Near-Infrared Photometric Variability of Stars Toward the Orion A Molecular Cloud' by Carpenter, John M.; Hillenbrand, Lynne A.; Skrutskie, M. F. 2001AJ....121.3160C",
+        # ra_cols, dec_cols
+        ['RAJ2000'], ['DEJ2000'],
+        #radec_fmt
+        'decimal degrees',
+        #name_col
+        '__CHS2001_')
+
+    
+    
+        
+        
+        
+
+
+
+    
