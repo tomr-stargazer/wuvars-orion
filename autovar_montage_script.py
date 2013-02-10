0f9ac7267be04e34a78a709c363b4823d89b79c3
Tom Rice <t.rice90@gmail.com>
2013-02-10 13:09:19 -0800

    Added high-variable capabilities

--- autovar_montage_script.py
+++ autovar_montage_script.py
@@ -19,6 +19,9 @@ from official_star_counter import *
 periodic_path = "/home/tom/reu/ORION/DATA/autovar/periodic/"
 periodic_path_g = "/home/tom/reu/ORION/DATA/autovar/periodic/glued/"
 nonper_path = "/home/tom/reu/ORION/DATA/autovar/nonperiodic/"
+hivar_path = "/home/tom/reu/ORION/DATA/autovar/hivar/"
+hivar_per_path = "/home/tom/reu/ORION/DATA/autovar/hivar/per/"
+hivar_per_path_g = "/home/tom/reu/ORION/DATA/autovar/hivar/glued/"
 
 # Relevant variable names:
 # ==Global==
@@ -32,6 +35,15 @@ nonper_path = "/home/tom/reu/ORION/DATA/autovar/nonperiodic/"
 # `autovars_strict_nonpers`: subset of `autovars_strict` who are non-periodic
 
 data = atpy.Table('/home/tom/reu/ORION/DATA/fdece_graded_clipped0.8_scrubbed0.1_dusted0.5.fits')
+
+
+# Let's make a function that does periods for "high-variables".
+
+hivar = autovars_true.where(autovars_true.Stetson > 20)
+hivar_periodics = autovars_true_periodics.where(
+    autovars_true_periodics.Stetson > 20)
+hivar_nonpers = autovars_true_nonpers.where(
+    autovars_true_nonpers.Stetson > 20)
 
 
 def gen_autovar_periodic_plots(start=0):
@@ -106,6 +118,86 @@ def gen_autovar_nonper_lc(start=0):
 
     return
 
+# START HIVAR HIJINKS HIJACK
+
+def gen_hivar_periodic_plots(start=0):
+    """ 
+    Makes glued 3-panel plots for all of the periodic hivars.
+    """
+    
+    counter = 1
+    c_max = len(hivar_periodics)
+
+    tables_of_periodics = [autovars_true_periods, autovars_true_periods_s1]
+
+    print 'lol'
+    for t in tables_of_periodics:
+        print 'lol2'
+        # first, the s123 periodics (more than 95% of them)
+        for s in t.SOURCEID[start:]:
+            # If this is a strict autovar, we give it special stuff.
+            if s not in hivar.SOURCEID: continue
+            if s in autovars_strict.SOURCEID:
+                flag = "s_"
+            else:
+                flag = ""
+
+            # Let's make 3 plots. LC, folded, and pgram. Save em all into a place.
+            plot3.graded_lc(data, s, abridged=True, color_slope=True, 
+                            timecolor=True, name=flag+str(s),
+                            outfile=hivar_per_path+"%s_lc.png"%str(s))
+
+
+            plot3.graded_phase(data, s, timecolor='time', 
+                               name=flag+str(s),
+                               period=t.best_period[t.SOURCEID==s], 
+                               color_slope=True, outfile=hivar_per_path+"%s_phase.png"%str(s))
+
+            try:
+                plot3.lsp_power(data, s, outfile=hivar_per_path+"%s_pgram.png"%str(s))
+            except Exception, e:
+                print "periodogram failed for %s" % str(s)
+                print e
+
+
+            # now glue em together!
+            call(["montage","-mode", "concatenate", "-tile", "2x", 
+                 hivar_per_path+"%s_*.png" % str(s), 
+                  hivar_per_path_g+flag+"%s-glued.png" % str(s) ])
+
+
+            print "Completed plot %d of %d" % (counter, c_max)
+            counter += 1
+        
+    return
+
+def gen_hivar_nonper_lc(start=0):
+    """ Makes lightcurves for nonperiodic hivars. """
+
+    counter = 1
+    c_max = len(hivar_nonpers)
+
+    for s in autovars_true_nonpers.SOURCEID[start:]:
+
+        if s not in hivar.SOURCEID: continue
+        # If this is a strict autovar, we give it special stuff.
+        if s in autovars_strict.SOURCEID:
+            flag = "s_"
+        else:
+            flag = ""
+
+        plot3.graded_lc(data, s, abridged=True, color_slope=True, 
+                        timecolor=True, name=flag+str(s),
+                        outfile=hivar_path+flag+"%s_lc.png"%str(s))
+
+        print "Completed plot %d of %d" % (counter, c_max)
+        counter += 1
+
+    return
+
+
+
+
 def gen_conf_periodic_plots(start=0):
     """ 
     Makes glued 3-panel plots for all the CONFIRMED subjective periodic variables.
@@ -147,5 +239,6 @@ def gen_conf_nonper_lc(start=0):
                         outfile=path6+"%s_lc.png"%str(s))
 
     return
+
 
 
