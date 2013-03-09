bfb6217b96eb7754a51ef60573ee50319dd42686
Tom Rice <t.rice90@gmail.com>
2013-03-09 02:19:54 -0800

    Wrote docstring/detailed spec for GCVS_period_get().

--- period_digger.py
+++ period_digger.py
@@ -23,8 +23,32 @@ from tablemate_script import (GCVS, Carpenter_2001,
 
 Carpenter2001_periods = atpy.Table(dpath+"Carpenter2001_datafile7.txt")
 
-def GCVS_period_get():
-    """ Gets a period for an input star from the GCVS table."""
+def GCVS_period_get(mated_table, primary_index, gcvs_index=False):
+    """ 
+    Gets a period for an input star from the GCVS table.
+
+    Parameters
+    ----------
+    mated_table : atpy.Table
+        Output of tablemate_script containing desired sources.
+        Must have a GCVS index column - this implies it was matched 
+        to GCVS already.
+    primary_index : int
+        The index (starting at zero) of the input star, in the mated_table.
+        Note: if `gcvs_index` == True, then enter the desired star's 
+        GCVS index here instead.
+    gcvs_index : bool, optional (default False)
+        if True, then `mated_table` is disregarded and `primary_index` 
+        is used as an index to the GCVS table directly.
+
+    Returns
+    -------
+    gcvs_period : float or None
+        The GCVS-listed Period for the input star.
+        If the star is not in the GCVS, or does not have a listed period,
+        then None is returned.
+
+    """
     pass
 
 def CHS01_period_get():
