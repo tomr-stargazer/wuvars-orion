cdbf8983776c1e0560f7c752fd003c45e64c9b2c
Tom Rice <t.rice90@gmail.com>
2013-03-14 00:13:57 -0400

    Intermediate commit. Writing and testing pieces of code.

--- clonekiller.py
+++ clonekiller.py
@@ -2,10 +2,15 @@
 This is a module for detecting and removing "cloned" sources in WFCAM data.
 
 A cloned source is one that has an identical (or identical modulo a few 
-datapoints) lightcurve as another star, and more importnatly, is at the 
+datapoints) lightcurve as another star, and more importantly, is at the 
 same physical location.
 
 """
+
+from __future__ import division
+
+import numpy as np
+
 
 def clone_flagger(table, max_offset=0.1):
     """
@@ -29,5 +34,28 @@ def clone_flagger(table, max_offset=0.1):
 
     """
 
+    # crude copy
+    flagged_table = table.where(table.RA > 0)
     
-    
+    flagged_table.sort('RA')
+
+    # convert max_offset to radians
+    max_offsetr = np.radians(max_offset / 3600)
+
+    clone = np.zeros(len(flagged_table),dtype=np.int64)
+
+    for i in range(1,len(flagged_table)):
+
+        # compare Dec and RA to the previous source
+        print np.degrees(np.abs(flagged_table.DEC[i] - flagged_table.DEC[i-1]))*3600
+        if np.abs(flagged_table.DEC[i] - flagged_table.DEC[i-1]) < max_offsetr:
+            print ("I think sources %s and %s are clones!" % 
+                   (flagged_table.SOURCEID[i], flagged_table.SOURCEID[i-1]) ) 
+
+        # if it's a clone, check if the previous guy was a clone
+
+        # if that was the case, follow that back further
+        
+        # else, the first guy back was the clone we're assigning
+        
+        if i > 20: break
