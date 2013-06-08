b6bb8cec3ef049f14e0e52ed1b485cd890c74bb4
Tom Rice <t.rice90@gmail.com>
2013-06-08 16:52:14 -0700

    Incremental commit.

--- ukvar_1226_inserter_oneoff.py
+++ ukvar_1226_inserter_oneoff.py
@@ -7,9 +7,13 @@ This is a one-off script to insert a single row into ukvar_spread.
 
 import numpy as np
 
-from official_star_counter import low_maxvars_spread, low_periodics
+from official_star_counter import low_periodics
 
 ukvar_spread = atpy.Table("/home/tom/Dropbox/Bo_Tom/aux_catalogs/UKvar_spreadsheet_withSIMBADnames.fits")
 
 # We're gonna take ukvar_spread, a new "table" with just one row corresponding 
-# to 
+# to ukvar #1226, custom tailored with the right columns, and APPEND them!
+
+# ID of UKvar 1226
+uk1226_id = 44199508514050
+
