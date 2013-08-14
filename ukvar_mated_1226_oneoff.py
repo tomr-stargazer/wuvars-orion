590e5fc0fd655baa4ee565113a64b935318826d7
Tom Rice <t.rice90@gmail.com>
2013-08-13 17:04:10 -0700

    Updated the oneoff to be aware that the Easties are removed.

--- ukvar_mated_1226_oneoff.py
+++ ukvar_mated_1226_oneoff.py
@@ -4,6 +4,9 @@ Creates the 42-column ukvar_mated table (1226-star version).
 This is the output of tablemate_script.UKvars_match(), with four
 columns appended from ukvar_spread_w1226.
 
+Update: now it also removes the "easties" (UKvar 1200-1225 minus 1203,1204)
+by default, since tablemate_script implicitly does that.
+
 """
 
 import numpy as np
@@ -24,5 +27,6 @@ for (col, name) in zip(missing_cols, missing_colnames):
     mated_ukvar.add_column(name, ukvar_spread[col], 
                            after="Rice2013_UKvars_index") 
 
+mated_ukvar.table_name = "Mated UKvar"
 
-mated_ukvar.write("/home/tom/Dropbox/Bo_Tom/aux_catalogs/ukvar_matched_table_withSIMBAD_w1226_2013_6_09.fits")
+mated_ukvar.write("/home/tom/Dropbox/Bo_Tom/aux_catalogs/ukvar_matched_table_withSIMBAD_w1226_minusEasties_2013_8_13.fits")
