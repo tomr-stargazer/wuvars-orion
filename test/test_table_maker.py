f1dc75666143ed169fd59511ca0656ed7cab23c7
Tom Rice <t.rice90@gmail.com>
2013-09-19 18:54:23 -0400

    Writing a test for convert_decimal_degree... -- it doesn't pass yet.

--- test/test_table_maker.py
+++ test/test_table_maker.py
@@ -13,4 +13,22 @@ def test_join_columns_with_plusminus():
     actual = join_columns_with_plusminus(test_values, test_errors, 3)
 
     assert (expected == actual).all()
-    
+
+def test_convert_decimal_degree_columns_to_sexagesimal():
+
+    test_ra_decimal_degrees = [123.456,
+                               98.765]
+    test_dec_decimal_degrees = [+78.9,
+                                -43.21]
+
+    expected_ra = np.array(['08:13:49.44',
+                            '06:35:03.60'])
+
+    expected_dec = np.array(['+78:54:00.00',
+                             '-43:12:36.00'])
+
+    actual_ra, actual_dec = convert_decimal_degree_columns_to_sexagesimal(
+        test_ra_decimal_degrees, test_dec_decimal_degrees)
+
+    assert (expected_ra == actual_ra).all()
+    assert (expected_dec == actual_dec).all()
