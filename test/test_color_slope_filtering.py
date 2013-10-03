9d800fa8b96f0c2fecbcdc49f31cad88fa3cd44d
Tom Rice <t.rice90@gmail.com>
2013-10-03 00:25:33 -0400

    Very basic, almost trivial, unit test.
    Any changes we are about to make? They have to preserve this test.

--- test/test_color_slope_filtering.py
+++ test/test_color_slope_filtering.py
@@ -1,1 +1,14 @@
 from __future__ import division
+
+from color_slope_filtering import *
+
+def test_that_old_arrays_dont_change():
+
+    assert len(jhk_filled) == len(jhk_empty) == 69
+
+    assert len(hk_filled) == 92
+    assert len(hk_empty) == 129
+
+    assert len(jh_filled) == 88
+    assert len(jh_empty) == 103
+
