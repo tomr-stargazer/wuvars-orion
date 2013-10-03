b6360ef6d932ac077e0cf0d61dd8d6dc0ab9c082
Tom Rice <t.rice90@gmail.com>
2013-10-03 01:52:14 -0400

    Wrote a second function that tests the slope_confidence parameter a bit.

--- test/test_color_slope_filtering.py
+++ test/test_color_slope_filtering.py
@@ -12,3 +12,13 @@ def test_that_old_arrays_dont_change():
     assert len(jh_filled) == 88
     assert len(jh_empty) == 103
 
+def test_filter_color_slopes():
+
+    fcs_jhk = filter_color_slopes(autovars_strict, 'jhk')
+
+    assert len(fcs_jhk) == 69
+
+    fcs_khk_halfy = filter_color_slopes(autovars_true, 'hk',
+                                        slope_confidence=0.5)
+    # I tested this guy by hand
+    assert len(fcs_khk_halfy) == 115
