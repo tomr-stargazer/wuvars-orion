14881940f6e723c8a118eb9bd6bbcd19f4f5fce1
Tom Rice <t.rice90@gmail.com>
2013-10-04 22:43:42 -0400

    Wrote filter_by_tile.

--- variables_data_filterer.py
+++ variables_data_filterer.py
@@ -4,5 +4,61 @@ Removes all stars from our source data except "final" variables.
 """
 
 from __future__ import division
+import os
 
+import numpy as np
+
+import atpy
+
+from tablemate_comparisons import ukvar_spread
+from official_star_counter import maxvars
+
+dropbox_bo_data = os.path.expanduser("~/Dropbox/Bo_Tom/data/")
+
+source_photometry = atpy.Table(
+    dropbox_bo_data + "fdece_graded_clipped0.8_scrubbed0.1_dusted0.5.fits")
+
+variables_photometry = source_photometry.where(
+    np.in1d(source_photometry.SOURCEID, ukvar_spread.SOURCEID))
+
+def filter_by_tile():
+
+    vp = variables_photometry
+
+    max_ra = maxvars.RA.max()
+    min_ra = maxvars.RA.min()
+    max_dec = maxvars.DEC.max()
+    min_dec = maxvars.DEC.min()
+
+    tile_size_ra = (max_ra - min_ra) / 4
+    tile_size_dec = (max_dec - min_dec) / 4
+
+    tile_tables = []
+    tile_spreadsheets = []
+    
+    for ra_i in range(4):
+
+        for dec_j in range(4):
+
+            tile_min_ra = min_ra + tile_size_ra*ra_i + tile_size_ra/10
+            tile_max_ra = min_ra + tile_size_ra*(ra_i+1) - tile_size_ra/10
+
+            tile_min_dec = min_dec + tile_size_dec*dec_j + tile_size_dec/10
+            tile_max_dec = min_dec + tile_size_dec*(dec_j+1) - tile_size_dec/10
+
+            tile_photometry = vp.where(
+                (vp.RA > tile_min_ra) & (vp.RA < tile_max_ra) &
+                (vp.DEC > tile_min_dec) & (vp.DEC < tile_max_dec) )
+
+            tile_spreadsheet = ukvar_spread.where(
+                (ukvar_spread.RA > tile_min_ra) & 
+                (ukvar_spread.RA < tile_max_ra) &
+                (ukvar_spread.DEC > tile_min_dec) & 
+                (ukvar_spread.DEC < tile_max_dec) )
+
+            tile_tables.append(tile_photometry)
+            tile_spreadsheets.append(tile_spreadsheet)
+            print len(set(tile_photometry.SOURCEID)), " stars in Tile %d, %d" % (ra_i,dec_j)
+
+    return tile_tables, tile_spreadsheets
 
