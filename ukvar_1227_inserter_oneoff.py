455438dd836b62e12d3697bf240c73cff27c9981
Tom Rice <t.rice90@gmail.com>
2014-04-07 02:57:22 -0400

    Wrote a functioning 1227 inserter!

--- ukvar_1227_inserter_oneoff.py
+++ ukvar_1227_inserter_oneoff.py
@@ -2,33 +2,31 @@
 Inserts UKvar #1227 into ukvar_spread.
 
 This is a one-off script to insert a single row into ukvar_spread. 
-It does some fancy things to (a) make sure all the values are correct, 
-especially the SIMBAD name; and (b) make sure the appending process goes 
-properly.
+It's a clone of ukvar_1226_inserter_oneoff.py.
 
-I've noticed that sometimes, when I run this multiple times in pylab, 
-it'll segfault. Just a note.
+I'll tweak things as necessary.
 
 """
 
+import os
 import numpy as np
 
 import atpy
 
-from official_star_counter import low_periodics
-from simbad_namer import simbad_namer
+from official_star_counter import subjectives
+#from simbad_namer import simbad_namer
 
-ukvar_spread = atpy.Table("/home/tom/Dropbox/Bo_Tom/aux_catalogs/UKvar_spreadsheet_withSIMBADnames.fits")
+ukvar_spread = atpy.Table(
+    os.path.expanduser(
+        "~/Dropbox/Bo_Tom/aux_catalogs/UKvar_spreadsheet_withSIMBADnames_w1226_minusEasties_renamedOldONCvarColumn.fits"))
 
 # We're gonna take ukvar_spread, a new "table" with just one row corresponding 
 # to ukvar #1227, custom tailored with the right columns, and APPEND them!
 
 # ID of UKvar 1227
-uk1227_id = 44199508514050
+uk1227_id = 44199508512491
 
-uk1227 = low_periodics.where(low_periodics.SOURCEID == uk1227_id)
-
-assert 1 == 2
+uk1227 = subjectives.where(subjectives.SOURCEID == uk1227_id)
 
 # now make uk1227 conform to ukvar_spread's 
 unwanted_columns = [x for x in uk1227.columns.keys 
@@ -46,24 +44,20 @@ print wanted_columns
 missing_cols = ['autovar',
                 'strict',
                 'periodic',
-                'ONCvar_ID',
+                'old_ONCvar_ID',
                 'SIMBAD_name',
                 'clone',
                 'UKvar_ID']
 
 missing_vals =  [0., # not automatic
                  0., # not strict
-                 1., # periodic!
-                 -1, # not an ONCvar -- unique among UKvars
-                 simbad_namer(uk1227)[0], # 'V* V2033 Ori'
+                 0., # not periodic! (although it technically is because EB, but we didn't find it as such.)
+                 -1, # has no "old" ONCvar ID -- only this and 1226 have this situation
+                 '2MASS J05351214-0531388', # simbad_namer(uk1227)[0],
                  0, # not a clone! I hope!
-                 1227] # 1225 existing UKvars.
+                 1227] # 1226 existing UKvars.
 
 missing_types = ['>f8', '>f8', '>f8', '>i8', '|S29', '>i8', '>i8']
-
-# table.add_column(name (str), data (array))
-#uk1227.add_column
-
 
 for (column, value, dtype) in zip(missing_cols, missing_vals, missing_types):
     uk1227.add_column(column, np.array([value]), dtype=dtype)
@@ -81,4 +75,10 @@ print uk1227.shape
 ukvar_spread_w1227 = ukvar_spread.where(ukvar_spread.SOURCEID > 0)
 ukvar_spread_w1227.append(uk1227)
 
-ukvar_spread_w1227.write("/home/tom/Dropbox/Bo_Tom/aux_catalogs/UKvar_spreadsheet_withSIMBADnames_w1227.fits")
+# don't write anything till I'm ready!
+assert 1==2
+
+ukvar_spread_w1227.write(
+    os.path.expanduser(
+        "~/Dropbox/Bo_Tom/aux_catalogs/UKvar_spreadsheet_withSIMBADnames_w1226_minusEasties_renamedOldONCvarColumn_w1227.fits"))
+
