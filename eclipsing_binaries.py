2c5a8f2f108e8c191669bbc58efcd28455178bb2
Tom Rice <t.rice90@gmail.com>
2014-04-07 14:32:58 -0400

    Wrote code to get the JHK columns from ukvar_spread for the EBs!

--- eclipsing_binaries.py
+++ eclipsing_binaries.py
@@ -14,6 +14,7 @@ import astropy.table
 
 import plot3
 from variables_data_filterer import source_photometry
+from tablemate_comparisons import ukvar_spread
 
 path = os.path.expanduser("~/Dropbox/Bo_Tom/data/")
 
@@ -22,6 +23,11 @@ eclipsing_binary_table = astropy.table.Table.read(
 
 # Choose the ones we numbered explicitly -- these are the only ones we have data for.
 relevant_EB_table = eclipsing_binary_table[eclipsing_binary_table['index'].mask == False]
+
+# Make sure I didn't screw up the ONCvar IDs
+
+for row in relevant_EB_table:
+	assert row['ONCvar ID'] == ukvar_spread.UKvar_ID[ukvar_spread.SOURCEID == row['WFCAM SOURCEID']]
 
 def make_lightcurves():
 
@@ -36,4 +42,25 @@ def make_lightcurves():
 
 	return figlist
 
+# JHK mag columns -- match our SOURCEIDs to ukvar_spread
+bool_array = np.in1d(ukvar_spread.SOURCEID, relevant_EB_table['WFCAM SOURCEID'].data.data)
+indices = np.where(bool_array)[0]
 
+indices_sorted_by_sourceid = indices[np.argsort(ukvar_spread.SOURCEID[indices])]
+indices_sorted_by_EB_number = indices_sorted_by_sourceid[
+	np.argsort( relevant_EB_table['index'].data.data[ # sort the EB numbers by SOURCEID and then get the inverse sort order
+		np.argsort(relevant_EB_table['WFCAM SOURCEID'])]     )]
+
+sourceid_column = ukvar_spread.SOURCEID[indices_sorted_by_EB_number]
+
+# make sure the sort went properly
+assert len(relevant_EB_table) == len(sourceid_column)
+for row, sid in zip(relevant_EB_table, sourceid_column):
+	assert row['WFCAM SOURCEID'] == sid
+
+j_column = ukvar_spread.j_meanr[indices_sorted_by_EB_number]
+h_column = ukvar_spread.h_meanr[indices_sorted_by_EB_number]
+k_column = ukvar_spread.k_meanr[indices_sorted_by_EB_number]
+
+jmh_column = ukvar_spread.jmh_meanr[indices_sorted_by_EB_number]
+hmk_column = ukvar_spread.hmk_meanr[indices_sorted_by_EB_number]
