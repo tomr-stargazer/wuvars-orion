191150dec31837a8b1c94ec15438b91390db0ce6
Tom Rice <t.rice90@gmail.com>
2014-04-10 02:56:38 -0400

    Improved & upgraded the script.

--- timescale_script.py
+++ timescale_script.py
@@ -9,28 +9,37 @@ from color_slope_temporal_experiment import *
 
 import matplotlib.pyplot as plt
 
-delta_t_list = [5,10,20, 25, 30, 40, 50, 75, 100,1000]
+#delta_t_list = [5,10,20, 25, 30, 40, 50, 75, 100]
+delta_t_list = [10, 40]
 
-def make_shuffle_dudes():
+def make_shuffle_dudes(n_iterations=3):
 
-	non_shuffled = calculate_color_slope_ratios_versus_time_baseline(
-		delta_t_list=delta_t_list, shuffle_dates=False)
+	list_of_non_shuffled = []
+
+	for i in range(n_iterations):
+		list_of_non_shuffled.append(
+			calculate_color_slope_ratios_versus_time_baseline(
+				delta_t_list=delta_t_list, date_offset=i*30, shuffle_dates=False) )
 
 	list_of_shuffle_dudes = []	
 
-	for i in range(5):
+	for i in range(n_iterations):
 		list_of_shuffle_dudes.append(
 			calculate_color_slope_ratios_versus_time_baseline(
 				delta_t_list=delta_t_list, shuffle_dates=True) )
 
-	return list_of_shuffle_dudes, non_shuffled
+	return list_of_shuffle_dudes, list_of_non_shuffled
 
-def plot_shuffle_dudes(list_of_shuffle_dudes, non_shuffled):
+def plot_shuffle_dudes(list_of_shuffle_dudes, list_of_non_shuffled):
 
 	fig1 = plt.figure()
 
-	plt.plot(non_shuffled.time_baseline, non_shuffled.n_positive_slope, 'r.')
-	plt.plot(non_shuffled.time_baseline, non_shuffled.n_negative_slope, 'b.')
+	plt.title("Non shuffled. Each run offset by 30 days.")
+
+	for non_shuffle in list_of_non_shuffled:
+
+		plt.plot(non_shuffle.time_baseline, non_shuffle.n_positive_slope, 'r.')
+		plt.plot(non_shuffle.time_baseline, non_shuffle.n_negative_slope, 'b.')
 
 	fig2 = plt.figure()
 
