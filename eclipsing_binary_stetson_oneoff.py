32d34f4b0922c933ff89dc4876004e21194d5829
Tom Rice <t.rice90@gmail.com>
2014-05-05 18:10:24 -0400

    Got stetson diff (before/after) for V1916 Ori

--- eclipsing_binary_stetson_oneoff.py
+++ eclipsing_binary_stetson_oneoff.py
@@ -6,6 +6,7 @@ This is a script that calculates the Stetson variability of eclipsing binaries
 
 from __future__ import division
 
+from spread3 import Stetson_machine
 from variables_data_filterer import source_photometry
 
 # V1916 Ori has SOURCEID: 44199508554632
@@ -14,4 +15,21 @@ v1916ori_data = source_photometry.where(source_photometry.SOURCEID == 4419950855
 # Period is 1.112403 days.
 v1916ori_period = 1.112403
 
-# I looked at the lightcurve and concluded that the eclipse 
+# I looked at the lightcurve and concluded that the eclipse occurs in these phases
+min_phase = 0.045
+max_phase = 0.95
+middle_phase_start = 0.465
+middle_phase_end = 0.55
+
+v1916ori_phase = ((v1916ori_data.MEANMJDOBS-54034) % v1916ori_period) / v1916ori_period
+
+v1916ori_out_of_eclipse = v1916ori_data.where((v1916ori_phase > min_phase) &
+	                                          (v1916ori_phase < max_phase) & 
+	                                          ((v1916ori_phase < middle_phase_start) |
+	                                           (v1916ori_phase > middle_phase_end)) )
+
+print len(v1916ori_out_of_eclipse)
+print "Datapoints removed: {0}".format(len(v1916ori_data) - len(v1916ori_out_of_eclipse))
+
+print "Stetson of V1916 Ori, full data: {0}".format(Stetson_machine(v1916ori_data, flags=256))
+print "Stetson of V1916 Ori out of eclipse: {0}".format(Stetson_machine(v1916ori_out_of_eclipse, flags=256))
