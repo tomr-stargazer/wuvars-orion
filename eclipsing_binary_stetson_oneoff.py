cf7fdb50237e370eefe509ed1ded3668bc41f8a0
Tom Rice <t.rice90@gmail.com>
2014-05-05 18:17:29 -0400

    Now I plot V1916 Ori before and after removing eclipse.

--- eclipsing_binary_stetson_oneoff.py
+++ eclipsing_binary_stetson_oneoff.py
@@ -7,10 +7,12 @@ This is a script that calculates the Stetson variability of eclipsing binaries
 from __future__ import division
 
 from spread3 import Stetson_machine
+import plot3
 from variables_data_filterer import source_photometry
 
 # V1916 Ori has SOURCEID: 44199508554632
-v1916ori_data = source_photometry.where(source_photometry.SOURCEID == 44199508554632)
+v1916ori_sid = 44199508554632
+v1916ori_data = source_photometry.where(source_photometry.SOURCEID == v1916ori_sid)
 
 # Period is 1.112403 days.
 v1916ori_period = 1.112403
@@ -33,3 +35,9 @@ print "Datapoints removed: {0}".format(len(v1916ori_data) - len(v1916ori_out_of_
 
 print "Stetson of V1916 Ori, full data: {0}".format(Stetson_machine(v1916ori_data, flags=256))
 print "Stetson of V1916 Ori out of eclipse: {0}".format(Stetson_machine(v1916ori_out_of_eclipse, flags=256))
+
+plot3.graded_phase(v1916ori_out_of_eclipse, v1916ori_sid, period=v1916ori_period, timecolor=True,
+	               name="V1916 Ori, eclipse data removed")
+
+plot3.graded_phase(v1916ori_data, v1916ori_sid, period=v1916ori_period, timecolor=True,
+	               name="V1916 Ori, with eclipses")
