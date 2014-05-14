febbd19747c5c0b9f5f0ad0e13500c045f030a75
Tom Rice <t.rice90@gmail.com>
2014-05-14 06:53:17 -0400

    I added a parameter: orion_abridger.

--- orion_plot.py
+++ orion_plot.py
@@ -6,8 +6,9 @@ Extensions to wuvars-proto/tr/plot4.py that are relevant to Orion.
 from __future__ import division
 
 from plot4 import StarData
+from orion_abridger import abridger as orion_abridger
 
 class OrionStarData(StarData):
 	def __init__(self, table, sid):
-		StarData.__init__(self, table, sid, date_offset=54034)
+		StarData.__init__(self, table, sid, date_offset=54034, abridger=orion_abridger)
 
