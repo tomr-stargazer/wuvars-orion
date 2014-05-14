aaa34d518812914f2e8b7203d743ef570af135ee
Tom Rice <t.rice90@gmail.com>
2014-05-14 12:34:11 -0700

    Added in a way to specify a star's name in OrionStarData.

--- orion_plot.py
+++ orion_plot.py
@@ -9,6 +9,6 @@ from plot4 import StarData
 from orion_abridger import abridger as orion_abridger
 
 class OrionStarData(StarData):
-	def __init__(self, table, sid):
-		StarData.__init__(self, table, sid, date_offset=54034, abridger=orion_abridger)
+	def __init__(self, table, sid, name=None):
+		StarData.__init__(self, table, sid, name=name, date_offset=54034, abridger=orion_abridger)
 
