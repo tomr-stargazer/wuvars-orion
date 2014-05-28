b61bfc1c82c891b824138a907a23eb30b45ca72e
Tom Rice <t.rice90@gmail.com>
2014-05-27 17:01:14 -1000

    Added generic function to find and print long-period lightcurves of given period span.

--- long_periods.py
+++ long_periods.py
@@ -15,6 +15,7 @@ from tablemate_comparisons import ukvar_spread
 from official_star_counter import maxvars_spread_per
 from variables_data_filterer import variables_photometry
 from lightcurve_maker import lc_and_phase_and_colors
+from table_maker import make_megeath_class_column
 
 
 # These can be broken into two groups:
@@ -137,7 +138,10 @@ def best_long_period( periodic_spread, min_period=50, max_period=200 ):
     return periodic_spread_updated
 
 
+megeath_class_column = make_megeath_class_column()
+
 path = os.path.expanduser("~/Dropbox/Bo_Tom/lightcurve_book/long_periods/")
+path2 = os.path.expanduser("~/Dropbox/Bo_Tom/lightcurve_book/short_periods/")
 
 def find_20_50_day_periodics_and_save_to_file():
 	blp = best_long_period(long_periodic_selector(maxvars_spread_per, max_period=100), max_period=100)
@@ -177,5 +181,20 @@ def find_20_50_day_periodics_and_save_to_file():
 	    fig.ax_j_phase.set_title("ONCvar {}".format(sd.name))
 	    fig.ax_j_lc.set_title(sid)
 	    fig.savefig("{0}/{1}/{2}.png".format(path, "10_20", sd.name))
+	    plt.close()
+
+def find_min_max_day_periodics_and_write(min_period=2, max_period=10, path=path2):
+	blp = best_long_period(long_periodic_selector(maxvars_spread_per, min_period=min_period, max_period=max_period),
+	                       min_period=min_period, max_period=max_period)
+	ukvar_blp = blp.where(np.in1d(blp.SOURCEID, ukvar_spread.SOURCEID))
+
+	print "There are {0} stars with nominal periods between {1} and {2}.".format(len(ukvar_blp), min_period, max_period)
+
+	for sid, period in zip(ukvar_blp.SOURCEID, ukvar_blp.best_period):
+	    sd = OrionStarData(variables_photometry, sid, ukvar_spread.UKvar_ID[ukvar_spread.SOURCEID==sid][0])
+	    fig = lc_and_phase_and_colors(sd, period)
+	    fig.ax_j_phase.set_title("ONCvar {} ({})".format(sd.name, megeath_class_column[ukvar_spread.SOURCEID==sid][0]))
+	    fig.ax_j_lc.set_title("{0}, Q={1}".format(sid, int((ukvar_spread.autovar+ukvar_spread.strict)[[ukvar_spread.SOURCEID==sid]][0])))
+	    fig.savefig("{0}/{1}_{2}/{3}.png".format(path, min_period, max_period, sd.name))
 	    plt.close()
 
