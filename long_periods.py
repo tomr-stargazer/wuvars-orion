f2e4b43f3baeb8137abaf6e6372acc8e0187d1a9
Tom Rice <t.rice90@gmail.com>
2014-05-31 15:19:32 -1000

    Moved the long_periodic_selector functions to periodic_selector!
    long_periods has more specialized digging in orion but periodic_selector is a generic thing so it's better.

--- long_periods.py
+++ long_periods.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from periodic_selector import long_periodic_selector, best_long_period
 from orion_plot import OrionStarData
 from tablemate_comparisons import ukvar_spread
 from official_star_counter import maxvars_spread_per
@@ -21,122 +22,6 @@ from table_maker import make_megeath_class_column
 # These can be broken into two groups:
 # 1. Anyone with a period longer than 20 days from the official periodics (shorter than 20 days)
 # 2. Long periods identified by hand greater than 50 days.
-
-def long_periodic_selector( spread, min_period=50, max_period=200 ):
-    """
-    Selects real periodic variables using periodogram parameters.
-
-    Currently only uses Lomb-Scargle input data. Soon to be fixed.
-
-    Parameters
-    ----------
-    spread : atpy.Table
-        Table of spreadsheet info on variable stars.
-
-    Returns
-    -------
-    periodic_spread : atpy.Table
-        Subset of `spread` with only "real" periodics.
-    
-    """
-
-    s = spread
-
-    r = s.where(
-        ( # J, H
-            (s.j_lsp_per < s.h_lsp_per * 1.05) &
-            (s.j_lsp_per > s.h_lsp_per * 0.95) &
-            (s.j_lsp_per > min_period) & (s.j_lsp_per < max_period) &
-            (s.j_lsp_pow > 12) & (s.h_lsp_pow > 12) 
-            ) |
-        ( # H, K
-            (s.h_lsp_per < s.k_lsp_per * 1.05) &
-            (s.h_lsp_per > s.k_lsp_per * 0.95) &
-            (s.h_lsp_per > min_period) & (s.h_lsp_per < max_period) &
-            (s.h_lsp_pow > 12) & (s.k_lsp_pow > 12) 
-            ) |
-        ( # J, K
-            (s.j_lsp_per < s.k_lsp_per * 1.05) &
-            (s.j_lsp_per > s.k_lsp_per * 0.95) &
-            (s.j_lsp_per > min_period) & (s.j_lsp_per < max_period) &
-            (s.j_lsp_pow > 12) & (s.k_lsp_pow > 12) 
-            ) |
-        ( #J
-            (s.j_lsp_pow > 15) & (s.j_lsp_per > min_period) & (s.j_lsp_per < max_period) 
-            ) |
-        ( #H
-            (s.h_lsp_pow > 15) & (s.h_lsp_per > min_period) & (s.h_lsp_per < max_period) 
-            ) |
-        ( #K
-            (s.k_lsp_pow > 15) & (s.k_lsp_per > min_period) & (s.k_lsp_per < max_period) 
-            ) 
-        )
-    
-    periodic_spread = r
-    return periodic_spread
-
-
-def best_long_period( periodic_spread, min_period=50, max_period=200 ):
-    """ 
-    A function that chooses the best period of the six returned.
-
-    Currently operates only on LSP input, just like periodic_selector.
-
-    Parameters
-    ----------
-    periodic_spread : atpy.Table
-        Table containing periodic variables
-
-    Returns
-    -------
-    periodic_spread_updated : atpy.Table
-        Input table with new "best_period" column added.
-
-    """
-
-    best_period = -1.*np.ones_like( periodic_spread.j_lsp_per )
-
-    r = periodic_spread
-
-    for i in range(len(periodic_spread)):
-        # J, H
-        if ((r.j_lsp_per[i] < r.h_lsp_per[i] * 1.05) and
-            (r.j_lsp_per[i] > r.h_lsp_per[i] * 0.95) and
-            (r.j_lsp_per[i] > min_period) and (r.j_lsp_per[i] < max_period) and
-            (r.j_lsp_pow[i] > 12) and (r.h_lsp_pow[i] > 12)
-            ):
-            best_period[i] = r.h_lsp_per[i]
-        # H, K
-        elif ((r.h_lsp_per[i] < r.k_lsp_per[i] * 1.05) and
-              (r.h_lsp_per[i] > r.k_lsp_per[i] * 0.95) and
-              (r.h_lsp_per[i] > min_period) and (r.h_lsp_per[i] < max_period) and
-              (r.h_lsp_pow[i] > 12) and (r.k_lsp_pow[i] > 12)
-              ):
-            best_period[i] = r.h_lsp_per[i]
-        # J, K
-        elif ((r.j_lsp_per[i] < r.k_lsp_per[i] * 1.05) and
-              (r.j_lsp_per[i] > r.k_lsp_per[i] * 0.95) and
-              (r.j_lsp_per[i] > min_period) and (r.j_lsp_per[i] < max_period) and
-              (r.j_lsp_pow[i] > 12) and (r.k_lsp_pow[i] > 12)
-              ):
-            best_period[i] = r.k_lsp_per[i]
-        # J
-        elif (r.j_lsp_pow[i] > 15) and (r.j_lsp_per[i] > min_period) and (r.j_lsp_per[i] < max_period):
-            best_period[i] = r.j_lsp_per[i]
-        # H
-        elif (r.h_lsp_pow[i] > 15) and (r.h_lsp_per[i] > min_period) and (r.h_lsp_per[i] < max_period):
-            best_period[i] = r.h_lsp_per[i]
-        # K
-        elif (r.k_lsp_pow[i] > 15) and (r.k_lsp_per[i] > min_period) and (r.k_lsp_per[i] < max_period):
-            best_period[i] = r.k_lsp_per[i]
-        else:
-            print "SOMETHING IS FISHY HERE"
-
-    r.add_column("best_period", best_period)
-
-    periodic_spread_updated = r
-    return periodic_spread_updated
-
 
 megeath_class_column = make_megeath_class_column()
 
--- periodic_selector.py
+++ periodic_selector.py
@@ -127,3 +127,119 @@ def best_period( periodic_spread ):
 
     periodic_spread_updated = r
     return r
+
+
+def long_periodic_selector( spread, min_period=50, max_period=200 ):
+    """
+    Selects real periodic variables using periodogram parameters.
+
+    Currently only uses Lomb-Scargle input data. Soon to be fixed.
+
+    Parameters
+    ----------
+    spread : atpy.Table
+        Table of spreadsheet info on variable stars.
+
+    Returns
+    -------
+    periodic_spread : atpy.Table
+        Subset of `spread` with only "real" periodics.
+    
+    """
+
+    s = spread
+
+    r = s.where(
+        ( # J, H
+            (s.j_lsp_per < s.h_lsp_per * 1.05) &
+            (s.j_lsp_per > s.h_lsp_per * 0.95) &
+            (s.j_lsp_per > min_period) & (s.j_lsp_per < max_period) &
+            (s.j_lsp_pow > 12) & (s.h_lsp_pow > 12) 
+            ) |
+        ( # H, K
+            (s.h_lsp_per < s.k_lsp_per * 1.05) &
+            (s.h_lsp_per > s.k_lsp_per * 0.95) &
+            (s.h_lsp_per > min_period) & (s.h_lsp_per < max_period) &
+            (s.h_lsp_pow > 12) & (s.k_lsp_pow > 12) 
+            ) |
+        ( # J, K
+            (s.j_lsp_per < s.k_lsp_per * 1.05) &
+            (s.j_lsp_per > s.k_lsp_per * 0.95) &
+            (s.j_lsp_per > min_period) & (s.j_lsp_per < max_period) &
+            (s.j_lsp_pow > 12) & (s.k_lsp_pow > 12) 
+            ) |
+        ( #J
+            (s.j_lsp_pow > 15) & (s.j_lsp_per > min_period) & (s.j_lsp_per < max_period) 
+            ) |
+        ( #H
+            (s.h_lsp_pow > 15) & (s.h_lsp_per > min_period) & (s.h_lsp_per < max_period) 
+            ) |
+        ( #K
+            (s.k_lsp_pow > 15) & (s.k_lsp_per > min_period) & (s.k_lsp_per < max_period) 
+            ) 
+        )
+    
+    periodic_spread = r
+    return periodic_spread
+
+
+def best_long_period( periodic_spread, min_period=50, max_period=200 ):
+    """ 
+    A function that chooses the best period of the six returned.
+
+    Currently operates only on LSP input, just like periodic_selector.
+
+    Parameters
+    ----------
+    periodic_spread : atpy.Table
+        Table containing periodic variables
+
+    Returns
+    -------
+    periodic_spread_updated : atpy.Table
+        Input table with new "best_period" column added.
+
+    """
+
+    best_period = -1.*np.ones_like( periodic_spread.j_lsp_per )
+
+    r = periodic_spread
+
+    for i in range(len(periodic_spread)):
+        # J, H
+        if ((r.j_lsp_per[i] < r.h_lsp_per[i] * 1.05) and
+            (r.j_lsp_per[i] > r.h_lsp_per[i] * 0.95) and
+            (r.j_lsp_per[i] > min_period) and (r.j_lsp_per[i] < max_period) and
+            (r.j_lsp_pow[i] > 12) and (r.h_lsp_pow[i] > 12)
+            ):
+            best_period[i] = r.h_lsp_per[i]
+        # H, K
+        elif ((r.h_lsp_per[i] < r.k_lsp_per[i] * 1.05) and
+              (r.h_lsp_per[i] > r.k_lsp_per[i] * 0.95) and
+              (r.h_lsp_per[i] > min_period) and (r.h_lsp_per[i] < max_period) and
+              (r.h_lsp_pow[i] > 12) and (r.k_lsp_pow[i] > 12)
+              ):
+            best_period[i] = r.h_lsp_per[i]
+        # J, K
+        elif ((r.j_lsp_per[i] < r.k_lsp_per[i] * 1.05) and
+              (r.j_lsp_per[i] > r.k_lsp_per[i] * 0.95) and
+              (r.j_lsp_per[i] > min_period) and (r.j_lsp_per[i] < max_period) and
+              (r.j_lsp_pow[i] > 12) and (r.k_lsp_pow[i] > 12)
+              ):
+            best_period[i] = r.k_lsp_per[i]
+        # J
+        elif (r.j_lsp_pow[i] > 15) and (r.j_lsp_per[i] > min_period) and (r.j_lsp_per[i] < max_period):
+            best_period[i] = r.j_lsp_per[i]
+        # H
+        elif (r.h_lsp_pow[i] > 15) and (r.h_lsp_per[i] > min_period) and (r.h_lsp_per[i] < max_period):
+            best_period[i] = r.h_lsp_per[i]
+        # K
+        elif (r.k_lsp_pow[i] > 15) and (r.k_lsp_per[i] > min_period) and (r.k_lsp_per[i] < max_period):
+            best_period[i] = r.k_lsp_per[i]
+        else:
+            print "SOMETHING IS FISHY HERE"
+
+    r.add_column("best_period", best_period)
+
+    periodic_spread_updated = r
+    return periodic_spread_updated
