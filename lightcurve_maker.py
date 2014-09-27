6c8d1da6079afed4c795c0da5db0a0037dcf9672
Tom Rice <t.rice90@gmail.com>
2014-09-27 01:25:09 -0400

    Added many many functions to make cool lightcurves!

--- lightcurve_maker.py
+++ lightcurve_maker.py
@@ -8,7 +8,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-from plot4 import lc_and_phase_and_colors, multi_lc_phase_colors, multi_lc_colors
+from plot4 import lc_and_phase_and_colors, multi_lc_phase_colors, multi_lc_colors, basic_lc
 from orion_plot import OrionStarData
 from tablemate_comparisons import ukvar_spread, ukvar_periods
 from variables_data_filterer import variables_photometry
@@ -46,6 +46,69 @@ orion_color_dic = {'red': ((0., 1, 0),
 
 orion_cmap = LinearSegmentedColormap('orion_cmap', orion_color_dic)
 
+
+def fivepanel_by_oncvar(oncvar, cmap=orion_cmap, **kwargs):
+	""" 
+	Makes a basic_lc plot 'smartly' based on an input ONCvar ID.
+
+	"""
+
+	sourceid = ukvar_spread['SOURCEID'][ukvar_spread['UKvar_ID'] == oncvar][0]
+
+	stardata = OrionStarData(variables_photometry, sourceid)
+
+	return basic_lc(stardata, time_cmap=cmap, **kwargs)
+
+def fivepanel_539(**kwargs):
+	fig = fivepanel_by_oncvar(539, **kwargs)
+
+	fig.ax_jhk.set_xlim(0.5,1.2)
+	fig.ax_jhk.set_ylim(0.75,1.5)
+
+	return fig
+
+def fivepanel_218(**kwargs):
+	fig = fivepanel_by_oncvar(218, **kwargs)
+
+	fig.ax_jhk.set_xlim(0.6, 2)
+	fig.ax_jhk.set_ylim(0.5, 2.5)
+
+	return fig
+
+def fivepanel_212(**kwargs):
+	fig = fivepanel_by_oncvar(212, **kwargs)
+
+	fig.ax_jhk.set_xlim(0.78, 1.7)
+	fig.ax_jhk.set_ylim(0.78, 1.68)
+
+	return fig
+
+def fivepanel_466(**kwargs):
+	fig = fivepanel_by_oncvar(466, **kwargs)
+
+	fig.ax_jhk.set_xlim(0.28, 0.58)
+	fig.ax_jhk.set_ylim(0.68, 0.98)
+
+	return fig
+
+def fivepanel_310(**kwargs):
+	fig = fivepanel_by_oncvar(310, **kwargs)
+
+	fig.ax_jhk.set_xlim(0.56, 0.94)
+	fig.ax_jhk.set_ylim(0.82, 1.51)
+
+	return fig
+
+def fivepanel_663(**kwargs):
+	fig = fivepanel_by_oncvar(663, **kwargs)
+
+	fig.ax_jhk.set_xlim(0.375, 0.84)
+	fig.ax_jhk.set_ylim(0.69, 1.25)
+
+	return fig
+
+
+
 def eightpanel_by_oncvar(oncvar, period=None, cmap=orion_cmap, **kwargs):
 	""" 
 	Makes a lc_and_phase_and_colors plot 'smartly' based on an input ONCvar ID.
@@ -73,7 +136,7 @@ def eightpanel_1226():
 def eightpanel_957(**kwargs):
 	return eightpanel_by_oncvar(957, offset=0.25, **kwargs)
 
-def aatau_tenpanel(**kwargs):
+def aatau_tenpanel(cmap=orion_cmap, **kwargs):
 
 	aatau_oncvar_ids = [130, 234, 295, 311, 334, 337, 371, 431, 553, 929]
 	offsets = [-0.2, -0.15, 0.5, 0.25, 0, -0.25, 0.5, 0.3, 0.1, 0.1]
@@ -85,7 +148,7 @@ def aatau_tenpanel(**kwargs):
 
 	bands = ['k']*10
 
-	aafig = multi_lc_phase_colors(aatau_stardatas, bands, aatau_periods, offsets, **kwargs)
+	aafig = multi_lc_phase_colors(aatau_stardatas, bands, aatau_periods, offsets, cmap=cmap, **kwargs)
 
 	for stardata, period, axes_dict in zip(aafig.stardatas, aafig.periods, aafig.axes_dicts):
 
@@ -102,9 +165,17 @@ def aatau_tenpanel(**kwargs):
 		ax_jhk.set_ylim(0,2)
 
 		aafig.canvas.draw()
+
+	aafig.axes_dicts[0]['phase'].set_title("Period-folded light curve", fontsize='small')
+	aafig.axes_dicts[0]['lc'].set_title("Light curve (MJD - 54034)", fontsize='small')
+	aafig.axes_dicts[0]['jhk'].set_title("$J-H$ vs. $H-K$", fontsize='small')
+	aafig.axes_dicts[0]['khk'].set_title("$K$ vs. $H-K$", fontsize='small')	
+
+	aafig.canvas.draw()
+
 	return aafig
 
-def longterm_nonperiodic_tenpanel(**kwargs):
+def longterm_nonperiodic_tenpanel(cmap=orion_cmap, **kwargs):
 
 	# compiled on 10 Sep 2010
 	longnononcvar_ids = sorted([1030, 30, 910, 1156, 148, 177, 28, 313, 605, 705])
@@ -117,7 +188,7 @@ def longterm_nonperiodic_tenpanel(**kwargs):
 
 	bands = ['k']*10
 
-	longnon_fig = multi_lc_colors(longnonstardatas, bands, **kwargs)
+	longnon_fig = multi_lc_colors(longnonstardatas, bands, cmap=cmap, **kwargs)
 
 	for stardata, axes_dict in zip(longnon_fig.stardatas, longnon_fig.axes_dicts):
 
@@ -133,9 +204,16 @@ def longterm_nonperiodic_tenpanel(**kwargs):
 		ax_jhk.set_ylim(0,2)
 
 		longnon_fig.canvas.draw()
+
+	longnon_fig.axes_dicts[0]['lc'].set_title("Light curve (MJD - 54034)", fontsize='small')
+	longnon_fig.axes_dicts[0]['jhk'].set_title("$J-H$ vs. $H-K$", fontsize='small')
+	longnon_fig.axes_dicts[0]['khk'].set_title("$K$ vs. $H-K$", fontsize='small')
+
+	longnon_fig.canvas.draw()
+
 	return longnon_fig
 
-def dipper_nonperiodic_fivepanel(**kwargs):
+def dipper_nonperiodic_fivepanel(cmap=orion_cmap, **kwargs):
 
 	# compiled on 10 Sep 2010
 	dipper_oncvar_ids = sorted([600, 165, 185, 423, 441])
@@ -148,7 +226,7 @@ def dipper_nonperiodic_fivepanel(**kwargs):
 
 	bands = ['k']*len(dipper_oncvar_ids)
 
-	dipper_fig = multi_lc_colors(dipper_stardatas, bands, **kwargs)
+	dipper_fig = multi_lc_colors(dipper_stardatas, bands, cmap=cmap, **kwargs)
 
 	for stardata, axes_dict in zip(dipper_fig.stardatas, dipper_fig.axes_dicts):
 
@@ -164,10 +242,17 @@ def dipper_nonperiodic_fivepanel(**kwargs):
 		ax_jhk.set_ylim(0,2)
 
 		dipper_fig.canvas.draw()
+
+	dipper_fig.axes_dicts[0]['lc'].set_title("Light curve (MJD - 54034)", fontsize='small')
+	dipper_fig.axes_dicts[0]['jhk'].set_title("$J-H$ vs. $H-K$", fontsize='small')
+	dipper_fig.axes_dicts[0]['khk'].set_title("$K$ vs. $H-K$", fontsize='small')	
+
+	dipper_fig.canvas.draw()
+
 	return dipper_fig
 
 
-def seven_longperiod_variables_bo(**kwargs):
+def seven_longperiod_variables_bo(cmap=orion_cmap, **kwargs):
 
 	sevenlong_oncvar_ids = [479, 149, 1226, 637, 802, 874, 665]
 	offsets = [0.5, -0.35, 0.12, -0.1, 0.3, 0.05, -0.05]
@@ -181,7 +266,7 @@ def seven_longperiod_variables_bo(**kwargs):
 
 	bands = ['k']*7
 
-	seven_fig = multi_lc_phase_colors(sevenlong_stardatas, bands, sevenlong_periods, offsets, **kwargs)
+	seven_fig = multi_lc_phase_colors(sevenlong_stardatas, bands, sevenlong_periods, offsets, cmap=cmap, **kwargs)
 
 	for stardata, period, axes_dict in zip(seven_fig.stardatas, seven_fig.periods, seven_fig.axes_dicts):
 
@@ -198,9 +283,17 @@ def seven_longperiod_variables_bo(**kwargs):
 		ax_jhk.set_ylim(0,2.5)
 
 		seven_fig.canvas.draw()
+
+	seven_fig.axes_dicts[0]['phase'].set_title("Period-folded light curve", fontsize='small')
+	seven_fig.axes_dicts[0]['lc'].set_title("Light curve (MJD - 54034)", fontsize='small')
+	seven_fig.axes_dicts[0]['jhk'].set_title("$J-H$ vs. $H-K$", fontsize='small')
+	seven_fig.axes_dicts[0]['khk'].set_title("$K$ vs. $H-K$", fontsize='small')	
+
+	seven_fig.canvas.draw()
+
 	return seven_fig
 
-def seven_clean_rotators(**kwargs):
+def seven_clean_rotators(cmap=orion_cmap, **kwargs):
 	rotator_oncvar_ids = [40, 359, 397, 906, 1068, 1091, 1165]
 	offsets = [0.5, 0.2, 0.1, 0.5, 0, 0.1, 0]
 
@@ -211,7 +304,7 @@ def seven_clean_rotators(**kwargs):
 
 	bands = ['k']*len(rotator_oncvar_ids)
 
-	rotator_fig = multi_lc_phase_colors(rotator_stardatas, bands, rotator_periods, offsets, **kwargs)
+	rotator_fig = multi_lc_phase_colors(rotator_stardatas, bands, rotator_periods, offsets, cmap=cmap, **kwargs)
 
 	for stardata, period, axes_dict in zip(rotator_fig.stardatas, rotator_fig.periods, rotator_fig.axes_dicts):
 
@@ -228,4 +321,12 @@ def seven_clean_rotators(**kwargs):
 		ax_jhk.set_ylim(0,2.5)
 
 		rotator_fig.canvas.draw()
+
+	rotator_fig.axes_dicts[0]['phase'].set_title("Period-folded light curve", fontsize='small')
+	rotator_fig.axes_dicts[0]['lc'].set_title("Light curve (MJD - 54034)", fontsize='small')
+	rotator_fig.axes_dicts[0]['jhk'].set_title("$J-H$ vs. $H-K$", fontsize='small')
+	rotator_fig.axes_dicts[0]['khk'].set_title("$K$ vs. $H-K$", fontsize='small')	
+
+	rotator_fig.canvas.draw()
+
 	return rotator_fig
