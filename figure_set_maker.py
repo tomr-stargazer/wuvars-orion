ae9ce11757c603cc5cc89a525241039956d6de74
Tom Rice <t.rice90@gmail.com>
2015-08-17 17:25:08 -0400

    making skeleton for figure set mockup

---
+++ figure_set_maker.py
@@ -0,0 +1,26 @@
+"""
+Makes figures for the AJ FigureSet.
+
+"""
+
+from plot4 import lc_and_phase_and_colors, multi_lc_phase_colors, multi_lc_colors, basic_lc, StarData
+from orion_plot import OrionStarData
+
+from variables_data_filterer import variables_photometry, source_photometry
+from tablemate_comparisons import ukvar_spread, ukvar_periods
+
+
+def figureset_page():
+    """ Creates a FigureSet page for a given star. """
+    pass
+
+
+def generate_mock_figureset_page():
+    """ Allows us to test the figureset pages. """
+    pass
+
+
+def generate_all_figureset_pages():
+    """ Generates all figureset pages."""
+    pass
+
